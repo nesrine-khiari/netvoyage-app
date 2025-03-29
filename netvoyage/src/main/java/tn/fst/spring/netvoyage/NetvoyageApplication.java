8b746f96d5894bcedfada03df30d205b2a9aed56
Nesrine <you@example.com>
2025-03-29 14:17:32 +0100

    Set up project

---
+++ netvoyage/src/main/java/tn/fst/spring/netvoyage/NetvoyageApplication.java
@@ -0,0 +1,13 @@
+package tn.fst.spring.netvoyage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetvoyageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetvoyageApplication.class, args);
+    }
+
+}
---
+++ netvoyage/src/test/java/tn/fst/spring/netvoyage/NetvoyageApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.fst.spring.netvoyage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetvoyageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
